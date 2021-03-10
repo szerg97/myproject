70c3246f789ad459ac634b0555dcd117668d006a
szerg97 <szerg97@gmail.com>
2021-03-10 18:12:06 +0100

    Q1 and Q2 done

--- src/Controller/TsvController.php
+++ src/Controller/TsvController.php
@@ -15,6 +15,15 @@ class TsvController{
 
     const PATH = "../templates/tsv/employees.csv";
 
+    /**
+     * @Route(path="list", name="getListAction")
+     */
+    public function getList(){
+        
+        $str = $this->getListHtml();
+        return new Response($str);
+    }
+
     private function readDatabase(): array{
         $database = array();
         $rows_array = file(self::PATH, FILE_IGNORE_NEW_LINES);
@@ -34,18 +43,36 @@ class TsvController{
         file_put_contents(self::PATH, $cont);
     }
 
+    private function queryHighestSalary(){
+        $db = $this->readDatabase();
+        $salaries = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 6) {
+                    array_push($salaries, $value);
+                }
+                $i++;
+            }
+        }
+
+        $idx = array_search(max($salaries), $salaries);
+        return $idx;
+    }
+
     private function getTable(){
         $database = $this->readDatabase();
-        $tpl_list = file_get_contents("../templates/tsv/list.html");
+        $tpl_table = file_get_contents("../templates/tsv/table.html");
         $tpl_rowsep = file_get_contents("../templates/tsv/rowsep.html");
         $tpl_cell = file_get_contents("../templates/tsv/cell.html");
 
         $rows = "";
 
-        foreach ($database as $key => $value) {
+        foreach ($database as $key => $rec) {
             $i = 0;
-            foreach ($value as $key2 => $value2) {
-                $rows .= str_replace("{{ CELL }}", $value2, $tpl_cell);
+            foreach ($rec as $key2 => $value) {
+                $rows .= str_replace("{{ CELL }}", $value, $tpl_cell);
             
                 if ($i % 10 == 9) {
                     $rows .= $tpl_rowsep;
@@ -55,17 +82,197 @@ class TsvController{
             }
         }
 
-        $tpl_list = str_replace("{{ ROWS }}", $rows, $tpl_list);
+        $tpl_table = str_replace("{{ ROWS }}", $rows, $tpl_table);
 
-        return $tpl_list;
+        return $tpl_table;
     }
 
-    /**
-     * @Route(path="list", name="getListAction")
-     */
-    public function getList(){
-        
+    private function getListHtml(){
         $str = $this->getTable();
-        return new Response($str);
+        $tp_list = file_get_contents("../templates/tsv/list.html");
+        $tp_list = str_replace("{{ TABLE }}", $str, $tp_list);
+
+        // Q1
+        $max_sal_val = max($this->getSalaries());
+        $max_idx = array_search($max_sal_val, $this->getSalaries());
+        $max_sal_name = $this->getNames()[$max_idx];
+        $tp_list = str_replace("{{ Q1 }}", $max_sal_name.", ".$max_sal_val, $tp_list);
+
+        //Q2
+        $ages = $this->getAges();
+        $average = array_sum($ages)/count(array_filter($ages));
+        $tp_list = str_replace("{{ Q2 }}", $average, $tp_list);
+
+        return $tp_list;
+    }
+
+    private function getNames(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 0) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getGenders(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 1) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getDates(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 2) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getAges(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 3) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getLevels(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 4) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getPositions(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 5) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getSalaries(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 6) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getAddresses(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 7) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getEmails(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 8) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
+    }
+
+    private function getPhones(){
+        $db = $this->readDatabase();
+        $array = array();
+
+        foreach ($db as $key => $rec) {
+            $i = 0;
+            foreach ($rec as $key2 => $value) {
+                if ($i == 9) {
+                    array_push($array, $value);
+                }
+                $i++;
+            }
+        }
+
+        return $array;
     }
 }
--- templates/tsv/readme.txt
+++ templates/tsv/readme.txt
@@ -1,2 +1,6 @@
 ################ EMPLOYEES ####################
-name,gender,dateOfBirth,age,levelOfEducation,position,address,email,phone
+name,gender,dateOfBirth,age,levelOfEducation,salary,position,address,email,phone
+
+Who has the highest salary and how much is it?
+What is the average age regarding employees?
+Which department has the most employees?
